f6a26570d3e015ac3ea97e150b998d070c87e1d6
Shim jae woon <jwun95@gmail.com>
2020-06-20 02:20:44 +0900

    첫번째 unittest

---
+++ student_management_system.py
@@ -0,0 +1,2 @@
+class StudentManagementSystem:
+    pass
--- test_student_managementsystem.py
+++ test_student_managementsystem.py
@@ -1,10 +1,12 @@
 import unittest
+from student_management_system import StudentManagementSystem
 
 class Test(unittest.TestCase):
 
 
-    def test_function(self):
-        pass
+    def test_constructor(self):
+        sms = StudentManagementSystem()
+        self.assertIsNotNone(sms)
 
 
 if __name__ == "__main__":
