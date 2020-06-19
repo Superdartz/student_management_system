b25f48a810c7f14f6386c987b3d13721a89d6062
Shim jae woon <jwun95@gmail.com>
2020-06-20 03:37:33 +0900

    unittest7: Student 객체에 모든 속성 입력 (아이디, 이름, 점수, 총점수, 평균)

--- student.py
+++ student.py
@@ -8,7 +8,33 @@ class Student:
         self._skor = float(items[2])
         self._seng = float(items[3])
         self._smat = float(items[4])
+        self._stotal = float(self._seng + self._skor + self._smat)
+        self._savg = float(self._stotal/3)
 
     @property
     def sid(self):
         return self._sid
+
+    @property
+    def sname(self):
+        return self._sname
+
+    @property
+    def skor(self):
+        return self._skor
+    
+    @property
+    def seng(self):
+        return self._seng
+    
+    @property
+    def smat(self):
+        return self._smat
+
+    @property
+    def stotal(self):
+        return self._stotal
+
+    @property
+    def savg(self):
+        return self._savg
--- test_student.py
+++ test_student.py
@@ -3,17 +3,46 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
+    def setUp(self):
+        self.my_student1 = Student("1,강호민,85,90,95")
+        self.my_student2 = Student("2,김광호,80,70,60")
+
+    def tearDown(self):
+        del self.my_student1
+        del self.my_student2
+
     def test_constructor(self):
-        my_student = Student("1,강호민,85,90,95")
-        self.assertIsNotNone(my_student)
+        self.assertIsNotNone(self.my_student1)
+        self.assertIsNotNone(self.my_student2)
 
     def test_sid(self):
-        my_student = Student("1,강호민,85,90,95")
-        self.assertEqual("1", my_student.sid)
+        self.assertEqual("1", self.my_student1.sid)
+        self.assertEqual("2", self.my_student2.sid)
 
-    def test_sid_2(self):
-        my_student = Student("2,김광호,80,70,60")
-        self.assertEqual("2", my_student.sid)
+    def test_sname(self):
+        self.assertEqual("강호민", self.my_student1.sname)
+        self.assertEqual("김광호", self.my_student2.sname)
+
+    def test_skor(self):
+        self.assertEqual(85, self.my_student1.skor)
+        self.assertEqual(80, self.my_student2.skor)
+
+    def test_seng(self):
+        self.assertEqual(90, self.my_student1.seng)
+        self.assertEqual(70, self.my_student2.seng)
+
+    def test_smat(self):
+        self.assertEqual(95, self.my_student1.smat)
+        self.assertEqual(60, self.my_student2.smat)
+
+    def test_stotal(self):
+        self.assertEqual(270, self.my_student1.stotal)
+        self.assertEqual(210, self.my_student2.stotal)
+
+    def test_savg(self):
+        self.assertEqual(90, self.my_student1.savg)
+        self.assertEqual(70, self.my_student2.savg)
+
 
 if __name__ == "__main__":
     unittest.main()
