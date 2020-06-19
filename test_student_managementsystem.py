e500afe1e76f7965b472fe39dad3628ea22949f3
Shim jae woon <jwun95@gmail.com>
2020-06-20 03:43:21 +0900

    test_student_management_system의 refactoring

--- test_student_managementsystem.py
+++ test_student_managementsystem.py
@@ -6,28 +6,29 @@ from unittest.mock import mock_open
 
 class Test(unittest.TestCase):
 
+    def setUp(self):
+        self.m_open1 = mock_open(read_data="1,강호민,85,90,95\n")
+        self.m_open2 = mock_open(read_data="1,강호민,85,90,95\n2,김광호,80,70,60\n")
 
     def test_constructor(self):
         sms = StudentManagementSystem()
         self.assertIsNotNone(sms)
 
     def test_read(self):
-        m_open = mock_open(read_data="1,강호민,85,90,95\n")
         
-        with patch('student_management_system.open', m_open):
+        with patch('student_management_system.open', self.m_open1):
             sms = StudentManagementSystem()
             self.assertEqual(1, sms.read('score.csv'))
 
-        m_open.assert_called_with('score.csv', 'rt', encoding="utf-8")
+        self.m_open1.assert_called_with('score.csv', 'rt', encoding="utf-8")
 
     def test_read_2(self):
-        m_open = mock_open(read_data="1,강호민,85,90,95\n2,김광호,80,70,60\n3,김민식,75,85,80\n")
         
-        with patch('student_management_system.open', m_open):
+        with patch('student_management_system.open', self.m_open2):
             sms = StudentManagementSystem()
-            self.assertEqual(3, sms.read('score.csv'))
+            self.assertEqual(2, sms.read('score.csv'))
 
-        m_open.assert_called_with('score.csv', 'rt', encoding="utf-8")
+        self.m_open2.assert_called_with('score.csv', 'rt', encoding="utf-8")
 
 
 
