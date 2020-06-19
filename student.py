5591b098c0cf7c487f06f3feccd98d561a91cc4a
Shim jae woon <jwun95@gmail.com>
2020-06-20 03:08:29 +0900

    unittest6: Student 생성 및 sid구현

--- student.py
+++ student.py
@@ -1,4 +1,14 @@
 class Student:
 
     def __init__(self, data):
-        pass
+        items = data.split(',')
+
+        self._sid = items[0]
+        self._sname = items[1]
+        self._skor = float(items[2])
+        self._seng = float(items[3])
+        self._smat = float(items[4])
+
+    @property
+    def sid(self):
+        return self._sid
--- test_student.py
+++ test_student.py
@@ -7,6 +7,13 @@ class TestStudent(unittest.TestCase):
         my_student = Student("1,강호민,85,90,95")
         self.assertIsNotNone(my_student)
 
+    def test_sid(self):
+        my_student = Student("1,강호민,85,90,95")
+        self.assertEqual("1", my_student.sid)
+
+    def test_sid_2(self):
+        my_student = Student("2,김광호,80,70,60")
+        self.assertEqual("2", my_student.sid)
 
 if __name__ == "__main__":
     unittest.main()
