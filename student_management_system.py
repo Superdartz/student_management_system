6e97f5ad52c695b4dba7586cbcf90b66705aef89
Shim jae woon <jwun95@gmail.com>
2020-06-20 17:58:04 +0900

    unittest8: key값으로 정렬, 평균값으로 정렬

--- student_management_system.py
+++ student_management_system.py
@@ -1,10 +1,52 @@
+from student import Student
+
+def key_stotal(item):
+    return item[1].stotal
 class StudentManagementSystem:
     def __init__(self):
-        pass
+        self._students = {}
 
     def read(self, student_data_file):
         with open(student_data_file, 'rt', encoding='utf-8') as fo:
             data = fo.read()
             lines = data.strip().split('\n')
 
-        return len(lines)
+        num = 0
+        for line in lines:
+            num = num + 1
+            self._students[num] = Student(line.strip())
+
+        return len(self._students)
+
+    def sort_by_reg(self, order="asc"):
+        if order == "asc":
+            sorted_students = sorted(self._students.items())
+        elif order == "des":
+            sorted_students = sorted(self._students.items(), reverse=True)
+
+        result = ""
+        for key, item in sorted_students:
+            result = result + str(key) + ","
+            result = result + item.sid + ","
+            result = result + item.sname + ","
+            result = result + str(int(item.skor)) + ","
+            result = result + str(int(item.seng)) + ","
+            result = result + str(int(item.smat)) + ","
+            result = result + str(int(item.stotal)) + ","
+            result = result + str(int(item.savg)) + "\n"
+        return result.strip()
+
+    def sort_by_stotal(self, order="asc"):
+        sorted_students = sorted(self._students.items(), key=key_stotal)
+
+        result = ""
+        for key, item in sorted_students:
+            result = result + str(key) + ","
+            result = result + item.sid + ","
+            result = result + item.sname + ","
+            result = result + str(int(item.skor)) + ","
+            result = result + str(int(item.seng)) + ","
+            result = result + str(int(item.smat)) + ","
+            result = result + str(int(item.stotal)) + ","
+            result = result + str(int(item.savg)) + "\n"
+        return result.strip()
--- test_student_managementsystem.py
+++ test_student_managementsystem.py
@@ -30,7 +30,38 @@ class Test(unittest.TestCase):
 
         self.m_open2.assert_called_with('score.csv', 'rt', encoding="utf-8")
 
+    def test_sort_by_reg_1(self):
+        with patch('student_management_system.open', self.m_open1):
+            sms = StudentManagementSystem()
+            sms.read('score.csv')
 
+            result = sms.sort_by_reg(order="asc")
+            self.assertEqual('1,1,강호민,85,90,95,270,90', result)
+
+    def test_sort_by_reg_2(self):
+        with patch('student_management_system.open', self.m_open2):
+            sms = StudentManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort_by_reg(order="asc")
+            self.assertEqual('1,1,강호민,85,90,95,270,90\n2,2,김광호,80,70,60,210,70', result)
+
+    def test_sort_by_reg_2_1(self):
+        with patch('student_management_system.open', self.m_open2):
+            sms = StudentManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort_by_reg(order="des")
+            self.assertEqual('2,2,김광호,80,70,60,210,70\n1,1,강호민,85,90,95,270,90', result)
+
+    def test_sort_by_stotal(self):
+        with patch('student_management_system.open', self.m_open2):
+            sms = StudentManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort_by_stotal(order="asc")
+            self.assertEqual('2,2,김광호,80,70,60,210,70\n1,1,강호민,85,90,95,270,90', result)
+         
 
 if __name__ == "__main__":
     unittest.main()
